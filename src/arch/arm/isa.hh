7bddb5047e72be0ac88a82edbfe6412a0c7e81e6
Gabe Black <gabe.black@gmail.com>
2021-08-14 02:50:25 -0700

    cpu,arch-arm: Use a sentry class valid for invalid RegIds.
    The default constructor for RegId would initialize it with the
    IntRegClass and register index 0. This is arbitrary and
    indistinguishable from a real ID to the first integer register.
    
    Instead, add a new class type constant InvalidRegClass, and use that to
    initialize an otherwise uninitialized RegId.
    
    Also, fill out some enums that needed to handle that value to silence
    compiler warnings.
    
    Change-Id: I3b58559f41adc1da5f661121225dbd389230e3af

--- src/arch/arm/isa.hh
+++ src/arch/arm/isa.hh
@@ -666,6 +666,8 @@ namespace ArmISA
                 return RegId(CCRegClass, flattenCCIndex(regId.index()));
               case MiscRegClass:
                 return RegId(MiscRegClass, flattenMiscIndex(regId.index()));
+              case InvalidRegClass:
+                return RegId();
             }
             return RegId();
         }
